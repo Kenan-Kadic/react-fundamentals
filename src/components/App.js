5abed9be0cee50e79bc9691769b7221341bfbfaf
Kenan Kadic <ken.kadic@outlook.com>
2022-01-07 11:54:55 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            Hello World <br />
+            My React confidence is growing now that I have done some state and props and fetched some APIs<br />
+            I even made a custom hook <br />
+            I called it useFetch and it combined useState with useEffect to fetch a API <br />
+            There is no going back now--I will be a React Developer, God willing <br />
+            Repetition makes perfect so I keep repeating <br />
+        </div>
+    )
+}
+export default App
