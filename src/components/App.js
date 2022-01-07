6829954d49c09e585e69b405340770269322a451
Kenan Kadic <ken.kadic@outlook.com>
2022-01-07 17:39:03 -0600

    useFetch.js added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import String from '../components/string/String';
 import Counter from '../components/counter/Counter';
+import useFetch from '../components/useFetch/useFetch';
 
 const App = () => {
 
+    const { data } = useFetch();
+    const { results } = data || [];
 
     return (
         <div className="App">   
             <String />
             <Counter />
+
+            {results && 
+            results.map((user) => {
+                console.log('USER OBJECT', user);
+                return (
+                    <ul key = {user.id.value}>
+                        <li>{user.name.first}</li>
+                        <li>{user.gender}</li>
+                        <li>{user.email}</li>
+                        <li>
+                            <img src = {data.results[0].picture.medium} alt='random user'/>
+                        </li>
+                    </ul>
+                );
+            })}
+            
         </div>
-    )
-}
+    );
+};
 export default App
