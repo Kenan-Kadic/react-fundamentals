ea5dd94ebf9183a11b4ca1475b8eadbd3dc0f948
Kenan Kadic <ken.kadic@outlook.com>
2022-01-07 13:07:44 -0600

    App.js updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const App = () => {
+
+    const myText = 'Kenan is learning React \n because Kenan loves React';
+    const mySplitText= myText.split('\n').map((text) => <p>{text}</p>);
+
     return (
-        <div>
-            Hello World <br />
-            My React confidence is growing now that I have done some state and props and fetched some APIs<br />
-            I even made a custom hook <br />
-            I called it useFetch and it combined useState with useEffect to fetch a API <br />
-            There is no going back now--I will be a React Developer, God willing <br />
-            Repetition makes perfect so I keep repeating <br />
+        <div className="App">
+            {mySplitText}
         </div>
     )
 }
